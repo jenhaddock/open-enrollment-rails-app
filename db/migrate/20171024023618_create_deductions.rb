ec73c9ac135bfe797ff5de9815becb1996e6e1c3
jenhaddock <jentx123@hotmail.com>
2017-10-23 21:46:01 -0500

    Deduction migration change

--- db/migrate/20171024023618_create_deductions.rb
+++ db/migrate/20171024023618_create_deductions.rb
@@ -2,6 +2,9 @@ class CreateDeductions < ActiveRecord::Migration[5.1]
   def change
     create_table :deductions do |t|
       t.string :name
+      t.boolean :is_percentage
+      t.boolean :is_flat
+      t.decimal :amount
 
       t.timestamps
     end
