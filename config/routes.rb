8bc538527fb430d4e8fcc7c008e99b17e7d37d68
jenhaddock <jentx123@hotmail.com>
2017-10-24 21:45:39 -0500

    Sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get '/auth/facebook/callback' => 'sessions#create'
-  
+
   root to: 'users#homepage'
   resources :users
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 
   resources :dependent_deduction_details
   resources :deduction_details
