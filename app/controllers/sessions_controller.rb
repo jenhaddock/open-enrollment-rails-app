dabf5b5e17fb041d97215db869b07074debdb88f
jenhaddock <jentx123@hotmail.com>
2017-10-24 21:25:50 -0500

    Session controller updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.email = auth['info']['email']
+    @user = User.find_by(email: params[:user][:email])
+    if !@user.nil? && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to 'signin'
     end
+  end
 
-    session[:user_id] = @user.id
-
-    render '/'
+  def destroy
+    if !session[:user_id].nil?
+      session.delete :user_id
+    end
+    redirect_to '/'
   end
 
   private
