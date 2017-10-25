e1caa1e2f2449a2b2445cf35afd285b1725fd7ef
jenhaddock <jentx123@hotmail.com>
2017-10-24 20:31:03 -0500

    User model updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :dependents
 
   has_many :deduction_details
   has_many :deductions, through: :deduction_details
+
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.admin ||= false
+    end
+      
 end
