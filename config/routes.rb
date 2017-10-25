3d5b47c38671012693369dd89e18c2cd7b9d4852
jenhaddock <jentx123@hotmail.com>
2017-10-24 21:41:04 -0500

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get '/auth/facebook/callback' => 'sessions#create'
+  
+  root to: 'users#homepage'
+  resources :users
 
   resources :dependent_deduction_details
   resources :deduction_details
   resources :dependents
   resources :deductions
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
