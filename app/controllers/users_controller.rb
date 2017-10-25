4a2c5ad5d02f2f24c29205ba34d7c77f882b5d64
jenhaddock <jentx123@hotmail.com>
2017-10-24 21:31:27 -0500

    User Controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
+  def homepage
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
