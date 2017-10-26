66e9d60af687ba480a0d09749dea49b757496cfd
jenhaddock <jentx123@hotmail.com>
2017-10-25 21:38:31 -0500

    total deductions helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :total_deductions
 
   def current_user
     session[:user_id]
   end
 
+  def total_deductions(user)
+    @user = User.find(user)
+    total = 0
+    @user.deduction_details.each.do |ded_det|
+      @deduction = Deduction.find(ded_det.deduction_id)
+      if @deduction.is_percentage?
+        total = total + (@user.salary * @deduction.amount * 0.01)
+      else
+        total = total + @deduction.amount
+      end
+    end
+  end
 end
