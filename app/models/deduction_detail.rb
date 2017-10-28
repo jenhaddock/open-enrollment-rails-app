761a575ecb88330e1a133d770680f3c2c0292cdb
jenhaddock <jentx123@hotmail.com>
2017-10-28 16:55:42 -0500

    Add percentage to deduction_details

--- app/models/deduction_detail.rb
+++ app/models/deduction_detail.rb
@@ -1,4 +1,6 @@
 class DeductionDetail < ApplicationRecord
   belongs_to :user
   belongs_to :deduction
+
+  validates :percentage, numericality: {less_than: 101}, allow_nil: true
 end
--- db/migrate/20171024023846_create_deduction_details.rb
+++ db/migrate/20171024023846_create_deduction_details.rb
@@ -3,6 +3,7 @@ class CreateDeductionDetails < ActiveRecord::Migration[5.1]
     create_table :deduction_details do |t|
       t.integer :user_id
       t.integer :deduction_id
+      t.integer :percentage
 
       t.timestamps
     end
