c2879901c28111f6203ab2befd2e6d5ab1e13edf
jenhaddock <jentx123@hotmail.com>
2017-10-30 20:04:23 -0500

    Update Session controller for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if !@user.nil? && @user.authenticate(params[:user][:password])
+    -- revist 
+    if params[:user][:password].nil?
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.email = auth['info']['email']
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to 'signin'
+      if !@user.nil? && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to 'signin'
+      end
     end
   end
 
