74b4a7728b81bc7560ca071a5818d5924c07a7f6
jenhaddock <jentx123@hotmail.com>
2017-10-31 20:38:01 -0500

    DRY changes to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,28 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #DRY it up
     if auth['uid']
       @user = User.find_or_create_by(id: auth['uid']) do |u|
         u.email = auth['info']['email']
       end
-      session[:user_id] = @user.id
-      if @user.setup_complete?
-        redirect_to user_path(@user)
-      else
-        redirect_to user_new(@user)
-      end
+      load_user_page
     else
       if !@user.nil? && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        if @user.setup_complete?
-          redirect_to user_path(@user)
-        else
-          redirect_to user_new(@user)
-        end
+        load_user_page
       else
         redirect_to 'signin'
       end
+    end
+  end
+
+  def load_user_page
+    session[:user_id] = @user.id
+    if @user.setup_complete?
+      redirect_to user_path(@user)
+    else
+      redirect_to user_new(@user)
     end
   end
 
