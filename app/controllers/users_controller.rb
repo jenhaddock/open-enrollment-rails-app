bd77a6ac259b95b8f0bb3b3b983add1683510736
jenhaddock <jentx123@hotmail.com>
2017-11-09 20:29:11 -0600

    User controller update fixes for deduction details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,9 +45,14 @@ class UsersController < ApplicationController
     @user.deduction_details.delete_all
     if @user.save
       params[:user][:deduction_details_attributes].each do |detail|
-        binding.pry
-        if detail.id == 1
-          deduction_detail.create(user_id => @user.id, deduction_id => detail.deduction_id, deduction_detail.percentage => detail.percentage)
+        if params[:user][:deduction_details_attributes][detail.to_s][:id] == "1"
+          temp_percentage = 0
+          if !params[:user][:deduction_details_attributes][detail.to_s][:percentage].nil?
+            temp_percentage = params[:user][:deduction_details_attributes][detail.to_s][:percentage]
+          end
+          DeductionDetail.create(:user_id => @user.id,
+                                  :deduction_id => params[:user][:deduction_details_attributes][detail.to_s][:deduction_id],
+                                  :percentage => temp_percentage)
         end
       end
       redirect_to user_path(@user)
@@ -57,7 +62,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(current_user)
+    @user = User.find(current_user.id)
     if !@user.setup_complete?
       redirect_to new_user_path
     end
@@ -72,6 +77,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :salary, :admin, :setup_complete,
                                  :dependents_attributes => [:name, :relation],
-                                 :deduction_detail_attributes => [:id, :percentage])
+                                 :deduction_detail_attributes => [:id, :details => [:id, :percentage]])
   end
 end
