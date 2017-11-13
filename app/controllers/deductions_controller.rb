508005c967e9cf44613529fdd04a480169e8d76c
jenhaddock <jentx123@hotmail.com>
2017-11-12 19:12:57 -0600

    Deduction New/Edit views

--- app/controllers/deductions_controller.rb
+++ app/controllers/deductions_controller.rb
@@ -1,6 +1,28 @@
 class DeductionsController < ApplicationController
 
   def new
+    @deduction = Deduction.new
+  end
+
+  def index
+    binding.pry
     @deductions = Deduction.all
   end
+
+  def create
+    @deduction = Deduction.new(deduction_params)
+    if @deduction.save
+      redirect_to 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+ private
+   def deduction_params
+     params.require(:deduction).permit(:name, :is_flat, :is_percentage, :amount)
+   end
 end
