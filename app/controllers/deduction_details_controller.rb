57923e66e769df669d8ea6048ef54cb0094b65ec
jenhaddock <jentx123@hotmail.com>
2017-11-13 21:41:24 -0600

    Initial Deduction Detail additions

--- app/controllers/deduction_details_controller.rb
+++ app/controllers/deduction_details_controller.rb
@@ -1,2 +1,16 @@
 class DeductionDetailsController < ApplicationController
+
+  def index
+    @deduction_details = current_user.deduction_details
+  end
+
+  def update
+    binding.pry
+    current_user.deduction_details.delete_all
+  end
+
+  private
+    def deduction_detail_params
+      params.require(:deduction_detail).permit(:id, :details => [:id, :percentage])
+    end
 end
