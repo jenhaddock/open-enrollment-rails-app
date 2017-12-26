01e370b3b63c8e48f5b54dfbb1131a90d16cbdd1
jenhaddock <jentx123@hotmail.com>
2017-12-25 21:19:56 -0600

    Remove un-needed comments

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,5 @@
 $(function(){
   $("a.user_index").on("click", function(e){
-//    $.ajax({
-//      method: 'GET',
-//      url: this.href
-//    }).done(function(response){
-//      $("div.users").html(response)
       $.get(this.href).success(function(json){
         var $ol = $("div.users ol")
         $ol.html("")
