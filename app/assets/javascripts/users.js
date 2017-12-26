2e25eb7250bf53c589af31a531ad911cae5cff4c
jenhaddock <jentx123@hotmail.com>
2017-12-25 20:26:24 -0600

    User Index AJAX request

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,11 @@
-
+$(function(){
+  $("a.user_index").on("click", function(e){
+    $.ajax({
+      method: 'GET',
+      url: this.href
+    }).done(function(response){
+      $("div.users").html(response)
+    });
+    e.preventDefault();
+  })
+})
