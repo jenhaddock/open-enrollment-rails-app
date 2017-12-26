92bc61e4888fb42f852b006ad642d3f1ae404386
jenhaddock <jentx123@hotmail.com>
2017-12-25 21:18:14 -0600

    Update HTML response for user index to JSON

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,18 @@
 $(function(){
   $("a.user_index").on("click", function(e){
-    $.ajax({
-      method: 'GET',
-      url: this.href
-    }).done(function(response){
-      $("div.users").html(response)
-    });
+//    $.ajax({
+//      method: 'GET',
+//      url: this.href
+//    }).done(function(response){
+//      $("div.users").html(response)
+      $.get(this.href).success(function(json){
+        var $ol = $("div.users ol")
+        $ol.html("")
+
+        json.forEach(function(user){
+          $ol.append("<li>" + user.content + "<li>");
+        })
+      })
     e.preventDefault();
   })
 })
