55ff09825892d6a25bfe8a59a24f37b5d7b9efed
jenhaddock <jentx123@hotmail.com>
2017-12-26 22:08:38 -0600

    Add User Serializer assocations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :first_name, :last_name
+
+  has_many :dependents
+  has_many :deduction_details
+  has_many :deductions, through: :deduction_details
 end
