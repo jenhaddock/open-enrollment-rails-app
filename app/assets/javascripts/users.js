59a85ca1e2293699ed903bbd156972cd93316ddf
jenhaddock <jentx123@hotmail.com>
2018-01-01 16:52:58 -0600

    Refactored JS dependent show and added prototype to render HTML

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,9 +17,24 @@ $(function(){
       var $ul = $("div.dependents ul")
       $ul.html("")
       json.forEach(function(dependent){
-        $ul.append("<li>" + dependent.name + "</li>")
+        let newDependent = new Dependent(dependent)
+        let dependentHTML = newDependent.formatDependent()
+        $ul.append(dependentHTML)
+    //    $ul.append("<li>" + dependent.name + " | " + dependent.relation +  "</li>")
       })
     })
     e.preventDefault();
   })
 })
+
+function Dependent(dependent){
+  this.id = dependent.id
+  this.name = dependent.name
+  this.relation = dependent.relation
+  this.user_id = dependent.user_id
+}
+
+Dependent.prototype.formatDependent = function() {
+  let dependentHTML = `<li> ${this.name} | ${this.relation} </li>`
+  return dependentHTML
+}
