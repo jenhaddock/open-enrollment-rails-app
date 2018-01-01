a5d40272ed6bb0c2ffbd44256e055166147ac6f4
jenhaddock <jentx123@hotmail.com>
2018-01-01 15:36:28 -0600

    Initial user dependent show JS function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,7 +4,20 @@ $(function(){
       var $ul = $("div.deductions ul")
       $ul.html("")
       json.forEach(function(deduction_details){
-        
+        debugger
+      })
+    })
+    e.preventDefault();
+  })
+})
+
+$(function(){
+  $("a.user_dependents").on("click", function(e){
+    $.get(this.href).success(function(json){
+      var $ul = $("div.dependents ul")
+      $ul.html("")
+      json.forEach(function(dependent){
+        $ul.append("<li>" + dependent.name + "</li>")
       })
     })
     e.preventDefault();
