224de3dac360b01284a569c7d15caa2bd624bbcc
jenhaddock <jentx123@hotmail.com>
2018-01-09 23:33:44 -0600

    Added JS for new dependent load

--- app/assets/javascripts/dependents.js
+++ app/assets/javascripts/dependents.js
@@ -1,5 +1,27 @@
 $(function (){
-  $("#new_dependent").on("submit", function(){
-    alert ("You clicked submit");
+  $("#newDependent").on("submit", function(e){
+    e.preventDefault();
+    debugger
+    $.ajax({
+      type: "POST",
+      url: "/dependents/create"
+      data: {}
+    })
+    var $ul = $("div.deductions ul")
+    let newDependent = new Dependent(dependent_details)
+    let dependentHTML = newDependent.formatDependent()
+    $ul.append(dependentHTML)
   })
 })
+
+function Dependent(dependent){
+  this.id = dependent.id
+  this.name = dependent.name
+  this.relation = dependent.relation
+  this.user_id = dependent.user_id
+}
+
+Dependent.prototype.formatDependent = function() {
+  let dependentHTML = `<li> ${this.name} | ${this.relation} </li>`
+  return dependentHTML
+}
